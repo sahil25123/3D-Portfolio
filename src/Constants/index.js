d623079dfb870bd64f156aba51afc963344b476e
sahil25123 <sahilgupta25123@gmail.com>
2025-05-17 14:30:45 +0530

    feat: add Navbar and LogoSection components; define navigation links and logo icons

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -10,3 +10,58 @@
   { value: 3, suffix: "+", label: "Internships & Trainings" },
   { value: 100, suffix: "+", label: "DSA Problems Solved" }
 ];
+
+ export const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+ export const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+   {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,16 @@
-
 import React from 'react'
-import Hero from '../Sections/HEro'
+import Hero from '../Sections/Hero'
 import Showcase from '../Sections/Showcase'
+import Navbar from '../Components/Navbar'
+import LogoSection from '../Components/LogoSection'
 
 const Home = () => {
   return (
-    <div className='min-h-screen'>
+    <div>
+      <Navbar/>
       <Hero/>
       <Showcase/>
-
-
-      
+      <LogoSection/>
     </div>
   )
 }
