13ed15bd7d5241a7f6af6c8e0ea8684ab6312af9
sahil25123 <sahilgupta25123@gmail.com>
2025-05-18 23:59:52 +0530

    fix: correct import path for experience cards constants

--- src/Sections/Journey.jsx
+++ src/Sections/Journey.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import { expCards } from "../constants";
+import { expCards } from "../Constants";
 import TitleHeader from "../Components/TitleHeader";
 import GlowCard from "../components/GlowCard";
 
